fde39ccbf3cce4fe75e61500122a595731b58d9a
Andy <otakushun@gmail.com>
2019-05-26 21:32:46 +0800

    new

---
+++ BzModel/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace BzModel.Repository
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+
+        T Select(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> Selects();
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void SaveChanges();
+
+    }
+}
